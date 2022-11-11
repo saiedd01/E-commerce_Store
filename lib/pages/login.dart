2a4c59e75a6b12c7fe032b505ea0e6de062c013b
saiedd01 <sa3edahmed20@gmail.com>
2022-11-11 11:30:59 +0200

    Saied- create login page

---
+++ lib/pages/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Text("data"),
+          ],
+        ),
+      ),
+    );
+  }
+}
