e4674583e4b95f5bc6a391876fc2f71bca944db7
saiedd01 <sa3edahmed20@gmail.com>
2022-11-11 12:55:13 +0200

    Saied- create shared colors

---
+++ lib/shared/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Btn_pink = Color.fromARGB(255, 241, 39, 100);
+const Btn_green = Color.fromARGB(255, 73, 179, 105);
+const appbar_Green = Color.fromARGB(255, 76, 141, 95);
