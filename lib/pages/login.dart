8bda0ed7d1bbdf92b9e0d529f6d1e20e63d9aabb
saiedd01 <sa3edahmed20@gmail.com>
2022-11-11 11:55:49 +0200

    Saied- Add textField

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -7,10 +7,34 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [
-            Text("data"),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(35),
+          child: Column(
+            children: [
+              const SizedBox(
+                  height: 70,
+              ),
+              TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  //obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Your E-mail : ",
+                    // To delete borders
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: Divider.createBorderSide(
+                          context,
+                      ),
+                    ),
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                      ),
+                    ),
+                    filled: true,
+                    contentPadding: const EdgeInsets.all(8),
+                  ))
+            ],
+          ),
         ),
       ),
     );
