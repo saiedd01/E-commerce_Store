c6c3c44aafa053835081a37037daa0fafb9b4ab5
saiedd01 <sa3edahmed20@gmail.com>
2022-11-11 13:15:01 +0200

    Saied- Add ElevatedButton

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/shared/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:e_commerce/shared/custom_textfield.dart';
 
@@ -11,22 +12,46 @@ class Login extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(35),
           child: Column(
-            children:const[
-              SizedBox(
+            children:[
+
+              const SizedBox(
                   height: 70,
               ),
+
               CTextField(
                 textInputTypeee: TextInputType.emailAddress,
                 isPassword: false,
                 hinttexttt: "Enter Your Email : ",
               ),
-              SizedBox(
+
+              const SizedBox(
                 height: 33,
               ),
+
               CTextField(
                 textInputTypeee: TextInputType.text,
                 isPassword: true,
                 hinttexttt: "Enter Your Password : ",
+              ),
+
+              const SizedBox(
+                height: 33,
+              ),
+
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  "Sign in",
+                   style:TextStyle(
+                      fontSize: 19
+                  ),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Btn_green),
+                  padding: MaterialStateProperty.all(const EdgeInsets.all(12)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8))),
+                ),
               ),
             ],
           ),
