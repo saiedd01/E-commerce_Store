652afb0e7e84225672cf00fe8712aadabc1a3466
saiedd01 <sa3edahmed20@gmail.com>
2022-11-11 16:05:22 +0200

    Saied- Create home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/pages/home.dart';
 import 'package:e_commerce/pages/login.dart';
 import 'package:e_commerce/pages/register.dart';
 import 'package:flutter/material.dart';
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: Home(),
     );
   }
 }
---
+++ lib/pages/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            "Home",
+        style: TextStyle(
+          fontSize: 16,
+        ),
+        ),
+
+
+      ),
+    );
+  }
+}
