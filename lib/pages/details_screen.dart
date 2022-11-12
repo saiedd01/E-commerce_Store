f26f4489c09989adc904c15f003509469a44f9db
saiedd01 <sa3edahmed20@gmail.com>
2022-11-12 09:27:20 +0200

    Saied- Create details screen

---
+++ lib/pages/details_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Details extends StatelessWidget {
+  const Details({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Text(
+          "details"
+      ),
+    );
+
+  }
+}
