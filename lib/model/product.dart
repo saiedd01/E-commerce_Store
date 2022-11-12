3f87b8ee5cfce1fd7bfc19383e3c8adeac6aa1b2
saiedd01 <sa3edahmed20@gmail.com>
2022-11-12 11:38:48 +0200

    Saied- Change price of product

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -10,19 +10,19 @@ class Product{
 }
 
 final List<Product> Products = [
-  Product(price: 12.5, imgPath: "assets/img/Ag.png"),
-  Product(price: 12.5, imgPath: "assets/img/G1.webp"),
-  Product(price: 12.5, imgPath: "assets/img/G1_1.png"),
-  Product(price: 12.5, imgPath: "assets/img/G3_3.png"),
-  Product(price: 12.5, imgPath: "assets/img/G3_2.png"),
-  Product(price: 12.5, imgPath: "assets/img/G3.png"),
-  Product(price: 12.5, imgPath: "assets/img/G4.png"),
-  Product(price: 12.5, imgPath: "assets/img/G4_2.png"),
-  Product(price: 12.5, imgPath: "assets/img/G5.png"),
-  Product(price: 12.5, imgPath: "assets/img/G5_1.png"),
-  Product(price: 12.5, imgPath: "assets/img/G5_2.png"),
-  Product(price: 12.5, imgPath: "assets/img/G6_3.png"),
-  Product(price: 12.5, imgPath: "assets/img/G7.png"),
-  Product(price: 12.5, imgPath: "assets/img/G8.png"),
+  Product(price: 25, imgPath: "assets/img/Ag.png"),
+  Product(price: 15, imgPath: "assets/img/G1.webp"),
+  Product(price: 12, imgPath: "assets/img/G1_1.png"),
+  Product(price: 19, imgPath: "assets/img/G3_3.png"),
+  Product(price: 25, imgPath: "assets/img/G3_2.png"),
+  Product(price: 12, imgPath: "assets/img/G3.png"),
+  Product(price: 15, imgPath: "assets/img/G4.png"),
+  Product(price: 17, imgPath: "assets/img/G4_2.png"),
+  Product(price: 12, imgPath: "assets/img/G5.png"),
+  Product(price: 15, imgPath: "assets/img/G5_1.png"),
+  Product(price: 15, imgPath: "assets/img/G5_2.png"),
+  Product(price: 13, imgPath: "assets/img/G6_3.png"),
+  Product(price: 15, imgPath: "assets/img/G7.png"),
+  Product(price: 12, imgPath: "assets/img/G8.png"),
 
 ];
